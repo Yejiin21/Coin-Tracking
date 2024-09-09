49da199e07def960cdd8748215c98495406d346e
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-09-09 13:40:23 +0900

    interface 파일 생성

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,21 +1,7 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 import ApexChart from "react-apexcharts";
-
-interface IHistorical {
-  time_open: string;
-  time_close: string;
-  open: number;
-  high: number;
-  low: number;
-  close: number;
-  volume: number;
-  market_cap: number;
-}
-
-interface ChartProps {
-  coinId: string;
-}
+import { ChartProps, IHistorical } from "../interface/interfaces";
 
 function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(
